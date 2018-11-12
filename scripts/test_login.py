d3f0a5b004c07888e61bec6496f649d0d69ccac5
liusheng003x <m15811179959@163.com>
2018-11-12 16:13:32 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,9 @@
+class TestContact:
+    def test_contact1(self):
+        assert 1
+    def test_contact2(self):
+        assert 1
+    def test_contact3(self):
+        assert 1
+    def test_contact4(self):
+        assert 0
